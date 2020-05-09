4b851ec2a116ee5e84f2e0b1c1e5f944f15d8a10
Axel Kee <axelshaded@gmail.com>
2020-05-09 22:08:01 +0800

    Initial Commit

---
+++ delegateSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  delegateSample
+//
+//  Created by Soulchild on 09/05/2020.
+//  Copyright © 2020 fluffy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
